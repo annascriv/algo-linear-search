2cd90d29a9428ed27c48bbcf1d3820b28d502c46
annascriv <scriven.anna@outlook.com>
2023-10-04 18:07:04 -0500

    finished global linear search

--- linearSearch.js
+++ linearSearch.js
@@ -8,8 +8,16 @@ function linearSearch(searchTerm, arr) {
   } return undefined
 }
 console.log(linearSearch(5,[1,2,5,6]))
-function globalLinearSearch(searchTerm, arr) {
-  return [];
-}
 
+
+function globalLinearSearch(searchTerm, arr) {
+  let indices = [];
+  for (i=0;i<arr.length;i++) {
+    if (arr[i]===searchTerm) {
+      indices.push(i)
+    } 
+  } return indices
+
+}
+console.log(globalLinearSearch("a", "bananas".split("")))
 module.exports = { linearSearch, globalLinearSearch };
