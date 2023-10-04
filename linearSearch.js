a8843254757984e2691842a6627b51edb87b5590
annascriv <scriven.anna@outlook.com>
2023-10-04 17:56:41 -0500

    linear search (first part) done

--- linearSearch.js
+++ linearSearch.js
@@ -1,7 +1,13 @@
 function linearSearch(searchTerm, arr) {
-  return undefined;
+  //let newArr = arr.split('');
+//console.log(newArr)
+  for (i=0;i<arr.length;i++) {
+    if (arr[i]===searchTerm) {
+      return i
+    } 
+  } return undefined
 }
-
+console.log(linearSearch(5,[1,2,5,6]))
 function globalLinearSearch(searchTerm, arr) {
   return [];
 }
